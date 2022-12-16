faaeaa37f50741dc41862e4549d67353d2b576a2
Sutsikii <nicolas-dutertre@bbox.fr>
2022-12-16 17:10:46 +0100

    Update

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
     $json_data = file_get_contents("./products.json");
     $info = json_decode($json_data);
 
-    // Pagination
-    @$page=$_GET["page"];
-    $nbr_articles = 8;
-    $nbr_pages = ceil($info[0]["info"]/$nbr_articles);
-    $debut=($page-1)*$nbr_articles;
+    // // Pagination
+    // @$page=$_GET["page"];
+    // $nbr_articles_par_pages = 8;
+    // $nbr_pages = ceil($info[0]["values"]/$nbr_articles);
+    // $debut=($page-1)*$nbr_articles;
 
 ?>
 
@@ -19,20 +19,33 @@
     <link rel="stylesheet" href="index.css">
     <title>Document</title>
 </head>
-<body>
+<body class="bg-gray-200">
+
+    <h1 class="text-center text-6xl py-7 text-white"> 
+        FakeTeeTurle
+    </h1>
 
 
-    <div class="bg-slate-900 grid grid-cols-3 grid-auto-flow ">
- 
+    <div class="max-w-2xl mx-auto px-4 py-8 lg:max-w-7xl grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-8 lg:grid-cols-3 xl-grid-cols-4">
+    
         <?php 
             foreach($info as $values)
             {
                 // echo($values[0] . '<br/>');
                 echo
                 ('
-                    <div>
-                    <p>' . $values[0] . '</p>
-                    <img src="' . $values[1] . '" alt="">
+                    <div class="bg-white shadow-lg rounded-lg">
+                        <img src="' . $values[1] . '" class="rounded-t-lg" alt="">
+                        <div class="p-4">
+                            <div class="flex mb-5">
+                                <h3 class="texte-2xl text-gray-700">
+                                    '. $values[0] .'
+                                </h3>
+                                <p class="mt-1 ml-auto text-lg font-medium text-gray-900">
+                                    
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 ');
             }
@@ -40,6 +53,8 @@
 
     </div>
         
+
+
     <script src="https://cdn.tailwindcss.com"></script>
 
 </body>
